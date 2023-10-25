d21f9f4f653d7d1d82a67b93138587f10476c6ce
JAGS <78938133+JoseAngelGarciaSanchez@users.noreply.github.com>
2023-10-25 10:31:25 +0200

    running without exposing the tf serving pod

--- app/main.py
+++ app/main.py
@@ -1,21 +1,38 @@
-# app.py
 import tornado.ioloop
 import tornado.web
+import tornado.httpclient
 import json
+
+TF_SERVING_URL = "http://tf-serving-service:443/v1/models/my_model:predict"
 
 
 class PreprocessHandler(tornado.web.RequestHandler):
     async def post(self):
         data = json.loads(self.request.body.decode("utf-8"))
         # TODO: Add preprocessing logic
-        preprocessed_data = data
-        self.write(json.dumps(preprocessed_data))
+        preprocessed_data = {"instances": [data]}
+
+        # Send data to TensorFlow Serving for prediction
+        response = await self.forward_request(TF_SERVING_URL, preprocessed_data)
+
+        self.write(response.body)
+
+    async def forward_request(self, url, data):
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        request = tornado.httpclient.HTTPRequest(
+            url=url,
+            method="POST",
+            body=json.dumps(data),
+            headers={"Content-Type": "application/json"},
+        )
+        response = await http_client.fetch(request)
+        return response
 
 
 def make_app():
     return tornado.web.Application(
         [
-            (r"/preprocess", PreprocessHandler),
+            (r"/predict", PreprocessHandler),
         ]
     )
 
